1b9a4829787ff33e729eebd12c6818cbe2c02f20
Sebb <sumr2@sumr.app>
2023-12-13 16:32:34 -0500

    First Commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '1020px',
+      xl: '1440px',
+    },
+    extend: {
+      colors: {},
+      fontFamily: {
+        sans: ['Tinos', 'serif'],
+      },
+    },
+  },
+  plugins: [],
+};
